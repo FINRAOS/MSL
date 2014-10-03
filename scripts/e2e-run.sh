1a3fd02b56eb8e927c39533fa94b4f0132ff5e7c
Bryan Robbins <bryantrobbins@gmail.com>
2014-10-03 13:45:25 -0400

    Change param name in setMockRespond Updating e2e to run browser tests

--- scripts/e2e-run.sh
+++
@@ -1,7 +0,0 @@
-# This script needs to be executed from the root directory.
-
-npm install msl-server-node
-./node_modules/msl-server/bin/msl --basedir=./msl-sample-app --port=8001 --debug=true &
-cd msl-client-java
-mvn clean verify
-kill -9 $!
---
+++ test/msl-client-browser/karma.conf.travis.js
@@ -0,0 +1,91 @@
+// Karma configuration
+// Generated on Fri Aug 29 2014 11:38:06 GMT-0400 (EDT)
+
+module.exports = function(config) {
+
+  var customLaunchers = {
+  	'SL_Chrome': {
+			base: 'SauceLabs',
+			browserName: 'chrome'
+		},
+
+		'SL_Firefox': {
+			base: 'SauceLabs',
+			browserName: 'internet explorer',
+			version: '9'
+		}
+	};
+
+
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+
+    // frameworks to use
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'lib/*.js',
+      '../../msl-client-browser/mockapi-browser.js',
+      '../../msl-client-browser/appcontainer-driver.js',
+      'spec/MSLTestSpec.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+      
+    ],
+
+
+    // test results reporter to use
+    reporters: ['progress','junit', 'saucelabs'],
+
+    // the default configuration
+    junitReporter: {
+      outputFile: 'test-results.xml',
+      suite: ''
+    },
+
+    // web server port
+    port: 8001,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    sauceLabs: {
+      testName: 'MSL Browser CI tests on OpenSauce'
+    },
+
+    captureTimeout: 120000,
+    customLaunchers: customLaunchers,
+
+    browsers: Object.keys(customLaunchers),
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true,
+
+    proxies: {
+      '/' : 'http://localhost:8002'
+    },
+
+    urlRoot: '/root',
+
+  });
+};
