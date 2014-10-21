64db44f6aef1c8829f73a49b4d7f35039b7d466f
Daniel Koo <danieljamin.koo@gmail.com>
2014-10-21 14:56:11 -0400

    Modified mockapi-browser.js to make all API calls synchronous
    Signed-off-by: Daniel Koo <danieljamin.koo@gmail.com>

--- msl-client-browser/mockapi-browser.js
+++ msl-client-browser/mockapi-browser.js
@@ -40,7 +40,7 @@ setMockRespond = function(server, port, configurations)
 		configurations['eval'] = configurations['eval'].toString();
 	}
 	
-    xmlHttp.open( 'POST', 'http://' + server + ':' + port + '/mock/fakerespond', true );
+    xmlHttp.open( 'POST', 'http://' + server + ':' + port + '/mock/fakerespond', false );
     xmlHttp.setRequestHeader('Content-Type', 'application/json');
     xmlHttp.send(JSON.stringify(configurations));
 }
@@ -65,7 +65,7 @@ registerTemplate = function(server, port, template, id)
     body.template= template;
     body.id = id;
 	
-    xmlHttp.open( 'POST', 'http://' + server + ':' + port + '/mock/template', true );
+    xmlHttp.open( 'POST', 'http://' + server + ':' + port + '/mock/template', false );
     xmlHttp.setRequestHeader('Content-Type', 'application/json');
 
     xmlHttp.send(JSON.stringify(body));
@@ -88,7 +88,7 @@ setInterceptXHR = function(server, port, requestPath)
 	var body = {};
     body.requestPath= requestPath;
 
-    xmlHttp.open( 'POST', 'http://' + server + ':' + port + '/mock/interceptxhr', true );
+    xmlHttp.open( 'POST', 'http://' + server + ':' + port + '/mock/interceptxhr', false );
     xmlHttp.setRequestHeader('Content-Type', 'application/json');
 
     xmlHttp.send(JSON.stringify(body));
@@ -115,7 +115,7 @@ getInterceptedXHR = function(server, port, requestPath, callback)
 	var body = {};
     body.requestPath= requestPath;
 
-    xmlHttp.open( 'POST', 'http://' + server + ':' + port + '/mock/getinterceptedxhr', true );
+    xmlHttp.open( 'POST', 'http://' + server + ':' + port + '/mock/getinterceptedxhr', false );
     xmlHttp.setRequestHeader('Content-Type', 'application/json');
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState == 4 && xmlHttp.status==200) {
@@ -143,7 +143,7 @@ setParamIgnored = function(server, port, params)
 	var body = {};
     body.requestPath= params;
 	
-    xmlHttp.open( 'POST', 'http://' + server + ':' + port + '/setIgnoreFlag', true );
+    xmlHttp.open( 'POST', 'http://' + server + ':' + port + '/setIgnoreFlag', false );
     xmlHttp.setRequestHeader('Content-Type', 'application/json');
     xmlHttp.send(JSON.stringify(body));
 }
@@ -163,7 +163,7 @@ unRegisterMock = function(server, port, requestPath)
 	var body = {};
     body.requestPath= requestPath;
 
-    xmlHttp.open( 'POST', 'http://' + server + ':' + port + '/unregisterMock', true );
+    xmlHttp.open( 'POST', 'http://' + server + ':' + port + '/unregisterMock', false );
     xmlHttp.setRequestHeader('Content-Type', 'application/json');
     xmlHttp.send(JSON.stringify(body));
 }
