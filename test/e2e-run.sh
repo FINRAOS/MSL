a9466fa6c571c23d7638e6fa5515aa577aebbc15
Liusongsen Yang <Liusongsen.Yang@finra.org>
2018-07-24 17:34:51 -0400

    fixed the java test

--- test/e2e-run.sh
+++ test/e2e-run.sh
@@ -47,7 +47,9 @@ node_modules/karma-cli/bin/karma start $TEST/msl-client-browser/karma.conf.js
 kill -9 $pid
 
 # Run Java e2e tests
-cd ../msl-sample-app
+echo "Current path"
+cd
+cd ./msl-sample-app
 ../node_modules/msl-server/bin/msl --port=8001 --debug=true &
 pid=`ps -ef | grep "msl" | head -n 1 | awk '{print $2}'`
 pushd $ROOT/msl-client-java
