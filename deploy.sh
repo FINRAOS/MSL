76af95b1a94db20249f319ae128e11d767a9884d
Liusongsen Yang <Liusongsen.Yang@finra.org>
2018-07-25 16:36:06 -0400

    added java deploy back

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,8 @@ echo "Attempting deployment..."
 if [ $TRAVIS_PULL_REQUEST == 'false' ]; then
   echo "Not a pull request, executing E2E script and deploying..."
   ./test/e2e-run.sh
-#  cd msl-client-java
-# mvn clean deploy -DskipTests --settings ../target/CM/settings.xml
+  cd msl-client-java
+  mvn clean deploy -DskipTests --settings ../target/CM/settings.xml
 else
   echo "Pull request, executing E2E script..."
   ./test/e2e-run.sh
